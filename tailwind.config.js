5f32b0ba4609f6e6d024a53760ffb3fc3908a679
ryo28 <wakisan1@ezweb.ne.jp>
2023-10-14 22:52:04 +0900

    ✨ カスタムでfontFamilyとdrop-shadow-whiteを追加しました

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      dropShadow: {
+        white: "5px 6px 20px rgba(255, 255, 255, 1)",
+      },
+      fontFamily: {
+        Helvetica: [
+          "Helvetica Neue",
+          "Arial",
+          "Hiragino Sans",
+          "Hiragino Kaku Gothic ProN",
+          "Meiryo",
+        ],
+      },
       textShadow: {
         sm: "0 1px 2px var(--tw-shadow-color)",
         DEFAULT: "0 2px 4px var(--tw-shadow-color)",
